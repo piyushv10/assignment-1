683d65f69ed53dda876d4e317ef38f3e9783188d
piyushv10 <vaidyapiyush8@gmail.com>
2023-12-29 15:57:51 +0530

    New files

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const productListElement = document.getElementById('productList');
-
-    // Fetch JSON data
+document.addEventListener('DOMContentLoaded', function () {
+    // Fetch JSON data from the API
     fetch('https://s3.amazonaws.com/open-to-cors/assignment.json')
         .then(response => response.json())
         .then(data => {
-            // Sort data based on descending popularity
+            // Sort data by descending popularity
             const sortedData = data.sort((a, b) => b.Popularity - a.Popularity);
 
-            // Display products
+            // Display data in the table
+            const tableBody = document.querySelector('#productTable tbody');
             sortedData.forEach(product => {
-                const productElement = document.createElement('div');
-                productElement.innerHTML = `
-                    <h3>${product.Title}</h3>
-                    <p>Price: $${product.Price}</p>
-                    <p>Popularity: ${product.Popularity}</p>
-                    <hr>
-                `;
-                productListElement.appendChild(productElement);
+                const row = tableBody.insertRow();
+                row.insertCell(0).textContent = product.Title;
+                row.insertCell(1).textContent = product.Price;
+                row.insertCell(2).textContent = product.Popularity;
             });
         })
         .catch(error => console.error('Error fetching data:', error));
